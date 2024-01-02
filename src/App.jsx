1ba8bf85ad0cbf49a982d572267bbd42d0114a03
mzsellem <marleysellem@gmail.com>
2024-01-02 11:13:54 -0500

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "../src/pages/home";
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+   return (
+      <>
+         <Routes>
+            <Route path="/" element={<Home />} />
+         </Routes>
+      </>
+   );
+}
