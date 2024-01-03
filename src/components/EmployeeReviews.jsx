2df2c972a08937a88fd599ee1c8d4813e75dcc2f
Anas Ahmed <anas31197@gmail.com>
2024-01-03 13:22:02 +0530

    Employee reviews (#1)
    * add material tailwind and start EmployeeReview component
    
    * work on employee review card
    
    ---------
    
    Co-authored-by: mzsellem <marleysellem@gmail.com>

---
+++ src/components/EmployeeReviews.jsx
@@ -0,0 +1,11 @@
+import { Carousel } from "@material-tailwind/react";
+import { EmployeeReviewsData } from "../data/EmployeeReviews";
+import EmployeeReviewCard from "../components/EmployeeReviewCard";
+
+export default function EmployeeReviews() {
+   return (
+      <>
+         <EmployeeReviewCard />
+      </>
+   );
+}
---
+++ src/data/EmployeeReviews.js
@@ -0,0 +1,23 @@
+export const EmployeeReviewsData = [
+    {
+        review: "“One of the best remote companies in the world. The managers invest a lot of time to make this company a great place to work.”",
+    },
+    {
+        review: "“They only work with A-class clients where you feel valued and grow and learn every day.”",
+    },
+    {
+        review: "“The culture at X-Team is top notch. They are always looking for ways to keep morale high.”",
+    },
+    {
+        review: "“Flexible company, awesome projects, and the best community.”",
+    },
+    {
+        review: "“Awesome community, great partners to work with, opportunities to improve and do your best!”",
+    },
+    {
+        review: "“I want to stay here until my retirement. I always feel supported by the company.”",
+    },
+    {
+        review: "“I've been developing for 14+ years & can honestly say I've never had the opportunity to work in such a positive atmosphere.”",
+    },
+]
