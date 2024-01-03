a8c2c4a3570c1346e85f9b945cef3e07513b873f
Anas Ahmed <anas31197@gmail.com>
2024-01-03 20:30:17 +0530

    first commit (#2)
    Co-authored-by: GoatSKR <sitesh.raulo.pj@gmail.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,12 @@
 import EmployeeReviews from "../components/EmployeeReviews";
+import EngineeringCompany from "../components/EngineeringCompany";
 
 export default function Home() {
    return (
       <>
+         <EngineeringCompany />
          <EmployeeReviews />
+
       </>
    );
 }
