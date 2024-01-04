12de095d7171cc291626d7f227f6ac5068b00d88
mzsellem <marleysellem@gmail.com>
2024-01-04 10:47:20 -0500

    start on component

---
+++ src/components/ScaleYourTeam.jsx
@@ -0,0 +1,25 @@
+export default function ScaleYourTeam() {
+    return (
+       <>
+          <div className="flex justify-center mb-4 text-center border">
+             <div className="text-6xl font-extrabold">
+                SCALE YOUR TEAM <br /> WITH SENIOR ENGINEERS <br /> FOR{" "}
+                <span className="text-greenstar">LASTING SUCCESS.</span>
+             </div>
+          </div>
+          <div className="grid grid-cols-2 grid-rows-3 gap-4">
+             <div className="border">
+                <div>
+                   <span className="font-extrabold">FLEXIBLE SCALING.</span>{" "}
+                   <br /> Ramp your team up or down with on-demand engineers. We
+                   make scaling simple with flexible contracts and monthly
+                   billing.
+                </div>
+             </div>
+             <div className="border">hi</div>
+             <div className="grid grid-cols-2 border">hey</div>
+             <div className="grid grid-cols-2 border">hello</div>
+          </div>
+       </>
+    );
+ } 
