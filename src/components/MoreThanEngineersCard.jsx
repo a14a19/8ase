e74907af45ef583f121ded516940aa7e18b9f5df
Anas Ahmed <anas31197@gmail.com>
2024-01-04 14:30:12 +0530

    added mtje section (#6)
    Co-authored-by: rahulsaamanth <rahulsaamanth@yahoo.com>

---
+++ src/components/MoreThanEngineersCard.jsx
@@ -0,0 +1,15 @@
+const MoreThanEngineersCard = ({ title, description, image }) => {
+  return (
+    <div className="bg-[#0c0c0c] h-[800px] w-full rounded-3xl overflow-hidden pt-12 mx-4">
+      <img src={image} alt="content" loading="lazy" className="pb-8" />
+      <h2 className="text-white text-3xl xl:text-4xl uppercase font-[900] px-12 py-10 text-wrap">
+        {title}
+      </h2>
+      <p className="text-gray-600 text-2xl xl:text-2xl px-12 font-medium">
+        {description}
+      </p>
+    </div>
+  )
+}
+
+export default MoreThanEngineersCard
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,15 +2,19 @@ import EmployeeReviews from "../components/EmployeeReviews";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import MoreThanEngineers from "../components/MoreThanEngineers"
 import { heroSectionHomeData } from "../data/heroSection";
 import EngineeringCompany from "../components/EngineeringCompany";
 
 
+
 export default function Home() {
+
    return (
       <>
          <Header />
          <Hero title={heroSectionHomeData.title} subTitle={heroSectionHomeData.subtitle} bgImg={heroSectionHomeData.backgroundImg} clientTitle={heroSectionHomeData.clientsTitle} />
+         <MoreThanEngineers />    
          <EngineeringCompany />
          <EmployeeReviews />
          <Footer />
