9bdc97f4b490f6a9438e5b538e3bf0ca22b2e065
a14a19 <anas31197@gmail.com>
2024-01-04 13:43:17 +0530

    hero, header, footer

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,16 @@
 import EmployeeReviews from "../components/EmployeeReviews";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import { heroSectionHomeData } from "../data/heroSection";
 
 export default function Home() {
    return (
       <>
+         <Header />
+         <Hero title={heroSectionHomeData.title} subTitle={heroSectionHomeData.subtitle} bgImg={heroSectionHomeData.backgroundImg} clientTitle={heroSectionHomeData.clientsTitle} />
          <EmployeeReviews />
+         <Footer />
       </>
    );
 }
