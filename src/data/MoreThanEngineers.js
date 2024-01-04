a93e09d17a50a3bbb34872a8b63404bd417d7c66
Anas Ahmed <anas31197@gmail.com>
2024-01-04 19:52:32 +0530

    Margin (#8)
    * spacing update
    
    * font margin update
    
    * font margin update
    
    * font margin update
    
    ---------
    
    Co-authored-by: rahulsaamanth <rahulsaamanth@yahoo.com>

--- src/data/MoreThanEngineers.js
+++ src/data/MoreThanEngineers.js
@@ -3,28 +3,28 @@ export const MoreThanEngineersData = [
     id: "1",
     title: "exceptional communication.",
     description:
-      "8asers excel in remote team communication, mastering the art of crafting comprehensive progress updates and adjusting expectations as required. They build trust through consistent actions and continuous communication.",
+      "8asers excel in remote team communication, adeptly providing detailed progress updates and adjusting expectations. They build trust through consistent actions and ongoing communication.",
     img: "https://x-team.com/_next/static/media/contentBoxImage01.df944ab6.webp",
   },
   {
     id: "2",
     title: "cultural compatibility.",
     description:
-      "Proactive, passionate, and open to new ideas, 8asers quickly acclimate to your tech infrastructure, team dynamics, and company culture, ensuring they become an integral team member from day one.",
+      "8asers are proactive, passionate, and receptive to new ideas. They swiftly adapt to your tech infrastructure, team dynamics, and company culture, seamlessly integrating as essential team members from day one.",
     img: "https://x-team.com/_next/static/media/contentBoxImage02.9412e942.webp",
   },
   {
     id: "3",
     title: "expert team enhancement.",
     description:
-      "With extensive experience collaborating alongside Fortune 500 engineering teams, 8asers bring invaluable expertise to help your organization scale and elevate its standards in quality and best practices.",
+      "With extensive collaboration experience in Fortune 500 engineering teams, 8asers bring invaluable expertise to enhance your organization's scalability and elevate standards in quality and best practices.",
     img: "https://x-team.com/_next/static/media/contentBoxImage03.d502aacb.webp",
   },
   {
     id: "4",
     title: "time-zone alignment.",
     description:
-      "We carefully select 8asers with the crucial time zone overlap needed for your team's seamless operation, recognizing its importance in fostering team morale, efficiency, and cooperation.",
+      "We meticulously choose 8asers with essential time zone overlap to ensure seamless team operations, acknowledging its pivotal role in fostering team morale, efficiency, and collaboration.",
     img: "https://x-team.com/_next/static/media/contentBoxImage04.b396c3d8.webp",
   },
 ]
