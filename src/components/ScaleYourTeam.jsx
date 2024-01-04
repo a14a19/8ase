514f613dbd7b0a2f81f7350e35eff00b53cb673e
mzsellem <marleysellem@gmail.com>
2024-01-04 13:29:59 -0500

    changes

--- src/components/ScaleYourTeam.jsx
+++ src/components/ScaleYourTeam.jsx
@@ -1,25 +1,32 @@
 export default function ScaleYourTeam() {
-    return (
-       <>
-          <div className="flex justify-center mb-4 text-center border">
-             <div className="text-6xl font-extrabold">
-                SCALE YOUR TEAM <br /> WITH SENIOR ENGINEERS <br /> FOR{" "}
-                <span className="text-greenstar">LASTING SUCCESS.</span>
-             </div>
-          </div>
-          <div className="grid grid-cols-2 grid-rows-3 gap-4">
-             <div className="border">
-                <div>
-                   <span className="font-extrabold">FLEXIBLE SCALING.</span>{" "}
-                   <br /> Ramp your team up or down with on-demand engineers. We
-                   make scaling simple with flexible contracts and monthly
-                   billing.
-                </div>
-             </div>
-             <div className="border">hi</div>
-             <div className="grid grid-cols-2 border">hey</div>
-             <div className="grid grid-cols-2 border">hello</div>
-          </div>
-       </>
-    );
- } 
+   return (
+      <>
+         <div className="flex justify-center mb-4 text-center border">
+            <div className="text-6xl font-extrabold">
+               SCALE YOUR TEAM <br /> WITH SENIOR ENGINEERS <br /> FOR{" "}
+               <span className="text-greenstar">LASTING SUCCESS.</span>
+            </div>
+         </div>
+         <div className="grid items-center w-full grid-cols-2 grid-rows-3 gap-8 border p-28">
+            <div className="flex border h-[25rem] justify-center w-[70%] text-2xl font-medium">
+               <div className="flex flex-col justify-center w-full p-12 border text-greyscale">
+                  <span className="text-4xl font-extrabold text-black">
+                     FLEXIBLE SCALING.
+                  </span>{" "}
+                  <br /> Ramp your team up or down with on-demand engineers. We
+                  make scaling simple with flexible contracts and monthly
+                  billing.
+                  <button className="w-1/2 p-4 mt-10 text-white bg-black border rounded-full">
+                     HIRE 8ASE
+                  </button>
+               </div>
+            </div>
+            <div className="flex border h-[25rem] w-full justify-center">
+               <img className="w-full rounded-xl" src="/8ase.png" alt="Logo" />
+            </div>
+            <div className="grid grid-cols-2 border">hey</div>
+            <div className="grid grid-cols-2 border">hello</div>
+         </div>
+      </>
+   );
+}
